4750c77dfefad6203bd7ecc054f1d760b199e310
Vishal Raj <vishalraj1802@gmail.com>
2023-12-20 13:48:46 -0500

    adds vimBeGood for practice

--- init.lua
+++ init.lua
@@ -72,10 +72,10 @@ require('lazy').setup({
   -- Git related plugins
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
-
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
+  'ThePrimeagen/vim-be-good',
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
